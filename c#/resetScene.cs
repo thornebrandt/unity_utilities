6f3fed55fc82bf10c163420411bec28d652f70f3
Thorne Brandt <thornebrandt@gmail.com>
2017-10-07 15:23:51 -0500

    reset scene script

---
+++ c#/resetScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class resetScene : MonoBehaviour {
+	void Update () {
+		checkKeyboardInput();
+	}
+  void checkKeyboardInput(){
+      if(Input.GetKeyDown("space")){
+          SceneManager.LoadScene(0);
+      }
+  }
+}
